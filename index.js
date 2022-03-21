3228637b27ea268520ebbefbd9e022a5b4d43ee2
soniagkalliath <soniageorge111@gmail.com>
2022-03-21 10:04:52 +0530

    Complete server without db

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express')
 
 const dataService = require('./services/data.service')
 
-//create an app using express
+const jwt = require('jsonwebtoken')
+
+//create an server app using express
 const app = express()
 
 //to pasrse json
@@ -36,8 +38,35 @@ app.delete('/',(req,res)=>{
     res.send("IT'S A DELETE METHOD")
 })
 
+// application specific middleware
+const appMiddleware = (req,res,next)=>{
+console.log("Application specific middleware")
+next()
+}
+
+app.use(appMiddleware)
 
 //bank app - API
+
+
+//to verify token - middleware
+const jwtMiddleware = (req,res,next)=>{
+try  {  
+    const token = req.headers["x-access-token"]
+    //verify token
+  const data =  jwt.verify(token,'supersecretkey123')
+    req.currentAcno = data.currentAcno
+    next()
+}
+    catch{
+        res.status(422).json({
+            statusCode:422,
+            status:false,
+            message:"Please Log In"
+        })
+    }
+
+}
 
 //register API
 app.post('/register',(req,res)=>{
@@ -54,9 +83,23 @@ app.post('/login',(req,res)=>{
 })
 
 //deposit API
-app.post('/deposit',(req,res)=>{
+app.post('/deposit',jwtMiddleware,(req,res)=>{
 
     const result = dataService.deposit(req.body.acno,req.body.password,req.body.amt)
+   res.status(result.statusCode).json(result)
+})
+
+//withdraw API
+app.post('/withdraw',jwtMiddleware,(req,res)=>{
+
+    const result = dataService.withdraw(req,req.body.acno,req.body.password,req.body.amt)
+   res.status(result.statusCode).json(result)
+})
+
+//transaction API
+app.post('/transaction',jwtMiddleware,(req,res)=>{
+
+    const result = dataService.getTransaction(req.body.acno)
    res.status(result.statusCode).json(result)
 })
 
--- services/data.service.js
+++ services/data.service.js
@@ -1,3 +1,6 @@
+//import jsonwebtoken
+const jwt = require('jsonwebtoken')
+
 database={
     1000:{acno:1000,uname:"Neer",password:1000,balance:5000,transaction:[]},
     1001:{acno:1001,uname:"Vyom",password:1001,balance:5000,transaction:[]},
@@ -43,12 +46,19 @@ database={
           if(password == database[acno]["password"]){
           currentAcno = acno
             currentUname = database[acno]["uname"]
+
+            //token generation
+const token = jwt.sign({
+  currentAcno:acno
+},'supersecretkey123')
+
                return {
           statusCode:200,
           status:true,
           message:"Successfully log in!!!!",
           currentAcno,
-          currentUname
+          currentUname,
+          token
         }
         }
         else{
@@ -104,10 +114,94 @@ database={
     }
   }
 
+  //withdraw definition
+
+  const withdraw = (req,acno,password,amt)=>{
+
+    var amount = parseInt(amt)
+
+    var currentAcno = req.currentAcno
+     if(acno in database){
+
+      if(password == database[acno]["password"]){
+
+        if(currentAcno == acno){
+          if(database[acno]["balance"]>amount){
+
+            database[acno]["balance"]-=amount
+            database[acno]["transaction"].push({
+              amount:amount,
+              type:"DEBIT"
+            })
+            console.log(database)
+           return {
+            statusCode:200,
+            status:true,
+            message:amount+" successfully debited.. And new balance is "+  database[acno]["balance"]
+          }  
   
+          }
+          else{
+              return {
+              statusCode:422,
+              status:false,
+              message:"Insufficient Balance"
+            }
+          }
+        }
+        else{
+          return {
+            statusCode:422,
+            status:false,
+            message:"Operation denied!!!"
+          }
+        }
+
+     
+       
+      }
+      else{
+        return {
+          statusCode:422,
+          status:false,
+          message:"Incorrect password"
+        }
+      }
+    }
+    else{
+      return {
+        statusCode:422,
+        status:false,
+        message:"User doesnot exist!!!!"
+      }
+    }
+  }
+
+  //transaction definition
+  const getTransaction = (acno)=>{
+
+    if(acno in database){
+     return {
+        statusCode:200,
+        status:true,
+       transaction : database[acno]["transaction"]
+      }  
+    }
+    else{
+      return {
+        statusCode:422,
+        status:false,
+        message:"User doesnot exist!!!!"
+      }
+    }
+
+  
+  }
 
   module.exports={
     register,
     login,
-    deposit
+    deposit,
+    withdraw,
+    getTransaction
   }
